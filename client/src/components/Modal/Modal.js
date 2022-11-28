d7d4ded59897ed042a782a9384565fff1f8ad1e2
Preet Mishra <ipreetmishra@gmail.com>
2022-11-28 22:54:32 +0530

    Update modal as per design specs.

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -5,8 +5,8 @@ export function Modal({ children }) {
   return (
     <Portal selector={"#root-modal"}>
       <ModalOverlay />
-      <div className="fixed inset-0 flex items-end md:items-center justify-center">
-        <div className="bg-white h-auto md:max-w-md rounded-t-2xl md:rounded-lg overflow-y-auto max-h-[90vh]">
+      <div className="fixed inset-0 flex items-center justify-center">
+        <div className="bg-white h-auto max-w-[90%] md:max-w-md rounded-lg overflow-y-auto max-h-[90vh]">
           {children}
         </div>
       </div>
--- client/src/components/Modal/ModalBody.js
+++ client/src/components/Modal/ModalBody.js
@@ -1,3 +1,3 @@
 export default function ModalBody({ children }) {
-  return <div className="pt-4 px-3 md:px-6 pb-16">{children}</div>;
+  return <div className="pt-4 px-3 md:px-6 pb-8 md:pb-16">{children}</div>;
 }
